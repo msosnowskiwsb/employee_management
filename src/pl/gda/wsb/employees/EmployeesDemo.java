037bb1d91d34a3c33789944d8d23580a41c1c376
Mateusz Sosnowski <sosnowski.mateusz88@gmail.com>
2020-01-28 21:52:42 +0100

    Zadanie numer 4 na branchu task4

--- src/pl/gda/wsb/employees/EmployeesDemo.java
+++ src/pl/gda/wsb/employees/EmployeesDemo.java
@@ -1,13 +1,59 @@
 package pl.gda.wsb.employees;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 
 public class EmployeesDemo {
 
-    public static void main(String args[]) {
-        System.out.print("Dzień dobry, ");
-        System.out.println(args[0]); // wymaga podania parametru Program arguments w Run Configuration
-        System.out.println("Aktualna data: " + new Date());
-    }
-}
+    static String companyName = "Logintegra Sp. z o. o.";
 
+    public static void main(String args[]) {
+
+        String operatorName = "Mateusz";
+        Integer employeesCounter = 14;
+
+        ArrayList<String> loggedEmployees = new ArrayList<>();
+        loggedEmployees.add("Jan Kowalski - kierownik");
+        loggedEmployees.add("Maria Ostrowska - dyrektor");
+        loggedEmployees.add("Anna Wolska - dyrektor");
+        loggedEmployees.add("Tomasz Jaworski - kierowca");
+        loggedEmployees.add("Roman Tadowski - sprzedawca");
+        loggedEmployees.add("Joanna Rafalska - sekretarka");
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder
+                .append(companyName).append("\n")
+                .append("Dzień dobry, ")
+                .append(operatorName).append("\n")
+                .append("Aktualna data: ")
+                .append(new Date());
+        System.out.println(stringBuilder);
+
+        if (employeesCounter == null) {
+            System.out.println("Błąd pobrania liczby pracowników!");
+        } else if (employeesCounter == 0) {
+            System.out.println("Brak pracowników");
+        } else {
+            System.out.println("Liczba pracowników: " + employeesCounter);
+        }
+
+        if (loggedEmployees.size() > 0) {
+            System.out.println("\nZalogowani pracownicy:");
+
+            Collections.sort(loggedEmployees);
+            int i = 0;
+            for (String employee : loggedEmployees) {
+                if (i++ == 5) {
+                    System.out.println("...");
+                    break;
+                }
+
+                System.out.println(employee);
+            }
+
+        }
+
+    }
+
+}
