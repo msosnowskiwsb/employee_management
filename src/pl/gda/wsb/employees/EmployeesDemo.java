8bc1cd9084ac0e154ed883f89293bbeec688ace5
Mateusz Sosnowski <sosnowski.mateusz88@gmail.com>
2020-01-28 20:56:51 +0100

    Task1

---
+++ src/pl/gda/wsb/employees/EmployeesDemo.java
@@ -0,0 +1,16 @@
+package pl.gda.wsb.employees;
+
+import java.util.Date;
+
+public class EmployeesDemo {
+
+
+    public static void main(String args[]) {
+
+        System.out.print("Dzień dobry, ");
+        //System.out.println(args[0]); // wymaga podania parametru Program arguments w Run Configuration
+        System.out.println("Aktualna data: " + new Date());
+
+    }
+}
+
