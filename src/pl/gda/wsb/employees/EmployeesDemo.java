48302f3d41bbf56e5dec92dce940af93462b177a
Mateusz Sosnowski <sosnowski.mateusz88@gmail.com>
2020-01-28 21:07:04 +0100

    Odkomentowanie fragmentu wyswietlajacego argumenty wejsciowe w main

--- src/pl/gda/wsb/employees/EmployeesDemo.java
+++ src/pl/gda/wsb/employees/EmployeesDemo.java
@@ -4,13 +4,10 @@ import java.util.Date;
 
 public class EmployeesDemo {
 
-
     public static void main(String args[]) {
-
         System.out.print("Dzień dobry, ");
-        //System.out.println(args[0]); // wymaga podania parametru Program arguments w Run Configuration
+        System.out.println(args[0]); // wymaga podania parametru Program arguments w Run Configuration
         System.out.println("Aktualna data: " + new Date());
-
     }
 }
 
