0dac81cb61aa153f456b3643b40ac739ffd914f1
Mateusz Sosnowski <sosnowski.mateusz88@gmail.com>
2020-01-29 07:27:21 +0100

    zadanie 2

--- src/pl/gda/wsb/employees/EmployeesDemo.java
+++ src/pl/gda/wsb/employees/EmployeesDemo.java
@@ -4,10 +4,21 @@ import java.util.Date;
 
 public class EmployeesDemo {
 
-    public static void main(String args[]) {
+    static String companyName = "Logintegra Sp. z o. o.";
+
+    public static void main(String[] args) {
+
+        String operatorName = "Mateusz";
+        int employeesCounter = 14;
+
+        System.out.println(companyName);
         System.out.print("Dzień dobry, ");
-        System.out.println(args[0]); // wymaga podania parametru Program arguments w Run Configuration
+        System.out.println(operatorName);
         System.out.println("Aktualna data: " + new Date());
+        System.out.println("Liczba pracowników: " + employeesCounter);
+
     }
+
 }
+
 
