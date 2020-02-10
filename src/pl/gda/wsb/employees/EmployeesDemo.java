b84a5d3da4a3424e431541fd1714ff6b51351a1f
Mateusz Sosnowski <msosnowski@wsb.gda.pl>
2020-02-11 00:27:54 +0100

    dodanie wyświetlania listy arg

--- src/pl/gda/wsb/employees/EmployeesDemo.java
+++ src/pl/gda/wsb/employees/EmployeesDemo.java
@@ -8,7 +8,7 @@ public class EmployeesDemo {
 
     public static void main(String[] args) {
 
-        String operatorName = "Mateusz";
+        String operatorName = args[0];
         int employeesCounter = 14;
 
         System.out.println(companyName);
@@ -16,6 +16,10 @@ public class EmployeesDemo {
         System.out.println(operatorName);
         System.out.println("Aktualna data: " + new Date());
         System.out.println("Liczba pracowników: " + employeesCounter);
+
+        for (int i=0; i<args.length; i++){
+            System.out.println("Argument [" + i + "]: " + args[i]);
+        }
 
     }
 
